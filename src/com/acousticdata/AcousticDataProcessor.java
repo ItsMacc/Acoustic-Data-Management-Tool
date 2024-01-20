dd65e14a98cf83be4bf9bbb1937c08cff2f37f03
mac <magicaltitan7@gmail.com>
2024-01-19 21:21:09 -0400

    Logging results after processing data

--- src/com/acousticdata/AcousticDataProcessor.java
+++ src/com/acousticdata/AcousticDataProcessor.java
@@ -1,13 +1,19 @@
 package com.acousticdata;
 
+import com.acousticdata.analysis.DataAnalyzer;
 import com.acousticdata.io.DataReader;
 
 import java.util.List;
+import java.util.Map;
 
 public class AcousticDataProcessor {
     public static void main(String[] args) {
 
         DataReader dataReader = new DataReader();
         List<AcousticDataSet> dataSet = dataReader.readData("src/com/acousticdata/dataset.txt");
+
+        DataAnalyzer dataAnalyzer = new DataAnalyzer(dataSet);
+        Map<String, Double> analyzedData = dataAnalyzer.analyzeData("11th January 2023");
+        System.out.println("Analysis Results: " + analyzedData);
     }
 }
